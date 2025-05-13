059fd954c7abe93ed78996141e3b95181122b2df
ARTHUR PINHEIRO MACHADO <arthur.machado@centralit.com.br>
2025-05-12 22:32:04 -0300

    altera nome dos campos de dias e horas

--- app/models/Reserva.py
+++ app/models/Reserva.py
@@ -6,10 +6,10 @@ from .Carro import Carro
 
 class Reserva(models.Model):
     id = models.AutoField(primary_key=True)
-    start_date = models.DateField(blank=False, null=False)
-    end_date = models.DateField(blank=False, null=False)
-    start_hour = models.TimeField(blank=False, null=False)
-    end_hour = models.TimeField(blank=False, null=False)
+    data_inicio = models.DateField(blank=False, null=False)
+    data_fim = models.DateField(blank=False, null=False)
+    hora_inicio = models.TimeField(blank=False, null=False)
+    hora_fim = models.TimeField(blank=False, null=False)
     carro = models.ForeignKey(Carro, on_delete=models.CASCADE)
     motorista = models.ForeignKey(Motorista, on_delete=models.CASCADE)
     
@@ -21,4 +21,4 @@ class Reserva(models.Model):
         managed = True
         verbose_name = "Reserva"
         verbose_name_plural = "Reservas"
-        ordering = ["-start_date"]
+        ordering = ["-data_inicio"]
