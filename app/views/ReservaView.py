578b1142d38b0372755005c70bd5f3c88488da0a
ARTHUR PINHEIRO MACHADO <arthur.machado@centralit.com.br>
2025-05-13 12:28:25 -0300

    adiciona o update de reserva

--- app/views/ReservaView.py
+++ app/views/ReservaView.py
@@ -24,3 +24,32 @@ class ReservaView(viewsets.GenericViewSet, mixins.CreateModelMixin, mixins.Destr
             return Response(status=400, data={"message": "Já existe uma reserva para este carro ou motorista nesta data"})
         reserva = Reserva.objects.create(data_inicio=data_inicio, data_fim=data_fim, carro=carro, motorista=motorista)
         return Response(ReservaSerializer(reserva, many=False).data)
+
+    def update(self, request, pk, *args, **kwargs):
+        reserva = Reserva.objects.get(id=pk)
+        try:
+            data_inicio = datetime.strptime(request.data['data_inicio'], '%d/%m/%Y_%H:%M')
+            reserva.data_inicio = data_inicio
+        except:
+            pass
+        
+        try:
+            data_fim = datetime.strptime(request.data['data_fim'], '%d/%m/%Y_%H:%M')
+            reserva.data_fim = data_fim
+        except:
+            pass
+        
+        try:
+            carro = Carro.objects.get(id=request.data['carro'])
+            reserva.carro = carro
+        except:
+            pass
+        
+        try:
+            motorista = Motorista.objects.get(id=request.data['motorista'])
+            reserva.motorista = motorista
+        except:
+            pass
+        
+        reserva.save()
+        return Response(ReservaSerializer(reserva, many=False).data)
