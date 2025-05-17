e2a51b4c534da2a74143f2e7274b04fecb128799
ARTHUR PINHEIRO MACHADO <arthur.machado@centralit.com.br>
2025-05-17 12:08:40 -0300

    Adiciona informaçoes do banco de dados via variaveis de ambiente

--- app/settings.py
+++ app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,11 +80,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'reserva_de_carro',
-        'USER': 'pi',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.environ.get("POSTGRES_DB"),
+        'USER': os.environ.get("POSTGRES_USER"),
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
+        'HOST': os.environ.get("POSTGRES_CONTAINER_NAME"),
+        'PORT': os.environ.get("POSTGRES_PORT"),
     }
 }
 
